00214063e21c154a8445fdd309f522b369e3f245
Hoàng Thị Minh Tâm <htmt1961@gmail.com>
2024-12-12 00:46:32 +0700

    Add .cs files

---
+++ Item.cs
@@ -0,0 +1,29 @@
+﻿public class Item
+{
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public double Discount { get; set; }
+
+    public Item() 
+    {
+        Name = "";
+        Price = 0;
+        Discount = 0;
+    }
+    public Item(string name, double price, double discount)
+    {
+        Name = name;
+        Price = price;
+        Discount = discount;
+    }
+
+    public double GetPrice()
+    {
+        return Price;
+    }
+
+    public double GetDiscount()
+    {
+        return Discount;
+    }
+}
