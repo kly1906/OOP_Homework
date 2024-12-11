d748521aeef57e9fa671e1117278f48c0853a5ff
Hoàng Thị Minh Tâm <162414914+kly1906@users.noreply.github.com>
2024-12-12 01:37:56 +0700

    HW1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fullName = "Hoang Thi Minh Tam";
+            string studentId = "12423030";
+            string className = "12423TN";
+            string githubUsername = "kly1906";
+            string email = "htmt1961@gmail.com";
+
+            Console.WriteLine($"{fullName}\t{studentId}\t{className}\t{githubUsername}\t{email}");
+
+            Console.ReadKey();
+        }
+    }
